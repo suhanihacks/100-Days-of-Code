e63af2a596e5910605222e6208070b169b73e3d8
Suhani goel <suhanigoel.07@gmail.com>
2025-10-06 09:59:18 +0530

    Create Day28_q2.c

---
+++ Day28_q2.c
@@ -0,0 +1,23 @@
+//Read and print elements of a one-dimensional array.
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int n, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
