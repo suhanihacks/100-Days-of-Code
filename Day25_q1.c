260195e62cd84da337ad868940f078b860f9dd56
Suhani goel <suhanigoel.07@gmail.com>
2025-10-06 09:56:25 +0530

    Create Day25_q1.c

---
+++ Day25_q1.c
@@ -0,0 +1,23 @@
+//Write a program to print the following pattern:
+//5
+//45
+//345
+//2345
+//12345
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows (n): ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = n - i + 1; j <= n; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
