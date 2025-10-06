88a8f927f5d18a2eb79bc4ed1e2adaee77b77f53
Suhani goel <suhanigoel.07@gmail.com>
2025-10-06 09:55:38 +0530

    Create Day23_q2.c

---
+++ Day23_q2.c
@@ -0,0 +1,25 @@
+//Write a program to print the following pattern:
+//*****
+//*****
+//*****
+//*****
+//*****
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= cols; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
